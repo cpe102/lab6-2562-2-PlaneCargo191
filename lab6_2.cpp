b0c05cd282eb0938cbe74112766a3c3b61762b3f
PlaneCargo191 <t.james_17@hotmail.com>
2020-01-17 17:05:44 +0700

    New

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,18 +1,32 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+using namespace std;
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker that is a really cool name.
-Fahsai: I think you are Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47.  I have a free movie tickets for you. 
-Fahsai: Let's go to cinema together!!! 
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watch Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/ 
-*/
+int main()
+{
+    string name;
+    int ID;
+    string movie;
+    string day;
+    string bye;
+
+    cout<<"Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout<<"?????: ";
+    getline(cin,name);
+    cout<<"Fahsai: Wow!!! "<<name<<" that is a really cool name.\nFahsai: I think you are Engineering student. What is your student ID?\n";
+    cout<<name<<": ";
+    cin>>ID;
+    cin.ignore();
+    cout<<"Fahsai: I think you may be GEAR "<<ID/10000000-12<<".  I have a free movie tickets for you.\nFahsai: Let's go to cinema together!!!\nFahsai: What movie do you want to watch?\n";
+    cout<<name<<": ";
+    getline(cin,movie);
+    cout<<"Fahsai: So....which day are you free to go with me?\n";
+    cout<<name<<": ";
+    getline(cin,day);
+    cout<<"Fahsai: "<<day<<"....that is OK!!! I'm looking forward to watch "<<movie<<" with you.\n";
+    cout<<name<<": ";
+    getline(cin,bye);
+    cout<<"Fahsai: 555+ see you "<<day<<". Bye Bye \(^ ^)/";
+
+    return 0;
+}
--- lab6_3.cpp
+++ lab6_3.cpp
@@ -1,8 +1,33 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+char before(char x)
+{
+	char y;
+
+	if (x<'A')
+	{
+		cout<<'0';
+	}
+	else
+	{
+		if (x>'Z')
+		{
+			cout<<'0';
+		}
+		else
+		{
+			if (x=='A')
+			{
+				cout<<'Z';
+			}
+			else
+			{
+			y=x-1;
+			}
+		}
+	}
+	return y;
 }
 
 int main(){
